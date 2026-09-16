880e93c3d8e151d10be65d266b4c0eafdecaa5cb
MuthuArjunan <marjunan@granitenet.com>
2026-09-16 18:33:22 -0400

    Create initial Product API

---
+++ tests/ProductApi.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductApi.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        
+
+    }
+}
---
+++ tests/ProductApi.UnitTests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace ProductApi.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            // This test should pass.
+            Assert.Equal(2, 1 + 1);
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            // This test is intended to fail.
+            Assert.Equal(3, 1 + 1);
+        }
+    }
+}
